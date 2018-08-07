a629398d1efce1e1eae41c11230c4418bf2e816d
Claytorpedo <claytorpedo@gmail.com>
2018-08-06 17:08:46 -0700

    Added multi-threaded solving.

--- Solitaire/batchrunner.cpp
+++ Solitaire/batchrunner.cpp
@@ -1,29 +1,42 @@
 #include "batchrunner.hpp"
 
+#include <atomic>
 #include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
+#include <thread>
+#include <chrono>
 
 #include "KlondikeSolver.hpp"
+#include "threadpool/threadpool/Threadpool.hpp"
 
 #include <direct.h>
 #include <errno.h>
-#include <chrono>
+
 using Clock = std::chrono::high_resolution_clock;
 
 using namespace solitaire;
 
 namespace {
-	const unsigned int BATCH_SIZE = 100;
+	std::atomic<u64> seedsRun = 0;
+
+	const unsigned int BATCH_SIZE = 10000;
+	const u64 MAX_STATES = 5000000;
+	const bool WRITE_GAMES = false;
 
 	const std::string RESULTS_DIR = "./results/";
 	const std::string SOLUTIONS_DIR = RESULTS_DIR + "/solutions/";
 	const std::string WIN_FILE = RESULTS_DIR + "winning_seeds.txt";
 	const std::string LOSE_FILE = RESULTS_DIR + "losing_seeds.txt";
 	const std::string UNKNOWN_FILE = RESULTS_DIR + "unknown_seeds.txt";
+
+	constexpr Threadpool::thread_num MAX_THREADS = 8;
+
+	Threadpool pool(MAX_THREADS);
+	std::vector<KlondikeSolver> solvers(MAX_THREADS, MAX_STATES);
 
 	// Pad input to a given width.
 	template<typename T>
@@ -47,6 +60,7 @@ namespace {
 			std::cerr << "Failed to create solutions directory.\n";
 			return false;
 		}
+
 		return true;
 	}
 
@@ -82,7 +96,8 @@ namespace {
 			switch (result.result) {
 			case(GameResult::Result::WIN):
 				winFile << PadWrite(result.seed) << " (positions tried: " << PadWrite(result.positionsTried, ' ') << ", solution length: " << PadWrite(result.solution.size(), ' ') << ")\n";
-				_write_solution_file(result);
+				if (WRITE_GAMES)
+					_write_solution_file(result);
 				break;
 			case(GameResult::Result::LOSE):
 				loseFile << PadWrite(result.seed) << " (positions tried: " << PadWrite(result.positionsTried, ' ') << ")\n";
@@ -93,29 +108,46 @@ namespace {
 			}
 		}
 	}
+
+	std::vector<GameResult> _batch_task(KlondikeSolver& solver, u64 startSeed, u64 numSeeds) {
+		std::vector<GameResult> results;
+		results.reserve(numSeeds);
+
+		const u64 seedEnd = startSeed + numSeeds;
+		for (u64 i = startSeed; i < seedEnd; ++i) {
+			solver.setSeed(i);
+			results.push_back(solver.solve());
+			++seedsRun;
+		}
+		return results;
+	}
 }
 
 bool batchrunner::run() {
 	if (!_startup())
 		return false;
 
-	std::vector<GameResult> results;
-	results.reserve(BATCH_SIZE);
-	KlondikeSolver solver(5000000);
+	std::vector<std::future<std::vector<GameResult>>> allResults;
 
 	const auto timeStart = Clock::now();
-	for (u32 i = 0; i < BATCH_SIZE; ++i) {
-		solver.setSeed(i);
-		const GameResult result = solver.solve();
-		results.push_back(result);
-		std::cout << "************************************************************************\n";
-		std::cout << "Seed: " << result.seed << " is " << (result.result == GameResult::Result::WIN ? "win" : result.result == GameResult::Result::LOSE ? "loss" : "unknown") << "\n";
-		if (result.result != GameResult::Result::UNKNOWN) {
-			std::cout << "num moves: " << result.positionsTried << "\n";
-		}
+	u64 startSeed = 0;
+	u64 seedsPerTask = BATCH_SIZE / solvers.size() + (BATCH_SIZE % solvers.size() != 0); // Round up.
+	for (auto& solver : solvers) {
+		allResults.push_back(pool.add(_batch_task, solver, startSeed, seedsPerTask));
+		startSeed += seedsPerTask;
 	}
-	_write_results(results);
+
+	while (!pool.isIdle()) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::cout << "\rSeeds Run: " << PadWrite<u64>(seedsRun, ' ');
+	}
+	std::cout << "\nDone. Writing results.\n";
+
+	for (auto& resultList : allResults)
+		_write_results(resultList.get());
+
 	const auto timeEnd = Clock::now();
-	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(timeEnd - timeStart).count() << " nanos\n";
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::seconds>(timeEnd - timeStart).count() << " seconds\n";
+
 	return true;
 }
